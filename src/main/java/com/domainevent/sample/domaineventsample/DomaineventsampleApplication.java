d36eb1ed6a2fdf35385277475d163022e6f83eb8
Haitham Shahin <hshahin@redhat.com>
2019-01-02 10:12:12 -0500

    first commit

---
+++ src/main/java/com/domainevent/sample/domaineventsample/DomaineventsampleApplication.java
@@ -0,0 +1,14 @@
+package com.domainevent.sample.domaineventsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaineventsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaineventsampleApplication.class, args);
+	}
+
+}
+
